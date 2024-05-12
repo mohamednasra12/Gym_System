41ec58e6f9bbfc4b4e4e2438a2d328c436b73707
mohamednasra12 <mohamednasra638@gmail.com>
2024-05-12 15:08:09 +0300

    fristcom

--- src/main.js
+++ src/main.js
@@ -6,5 +6,14 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "normalize.css";
 import "@fortawesome/fontawesome-svg-core";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/aura-dark-lime/theme.css";
 
-createApp(App).use(store).use(router).mount("#app");
+import Tooltip from "primevue/tooltip";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(PrimeVue)
+  .directive("tooltip", Tooltip)
+  .mount("#app");
