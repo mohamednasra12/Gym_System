7a45ecd438a08a5a8bf5137967f5ccc34bcda75b
mohamednasra12 <mohamednasra638@gmail.com>
2024-05-15 09:41:22 +0300

    nasra

--- src/components/Global/AppFooter.vue
+++ src/components/Global/AppFooter.vue
@@ -2,16 +2,12 @@
   <!-- Footer -->
   <footer class="text-center text-lg-start">
     <!-- Section: Social media -->
-    <section class="d-flex justify-content-center justify-content-lg-between">
-      <!-- Right -->
 
-      <!-- Right -->
-    </section>
     <!-- Section: Social media -->
 
     <!-- Section: Links  -->
-    <section class="">
-      <div class="container text-center text-md-start mt-5">
+    <section class="container-fluid">
+      <div class="text-center text-md-start mt-5">
         <!-- Grid row -->
         <div class="row mt-3">
           <!-- Grid column -->
@@ -131,28 +127,6 @@
         </router-link>
       </div>
       <h4>© All rights reserved @ gym platform 2024</h4>
-    </div>
-
-    <!-- Copyright -->
-    <div>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
-      </a>
     </div>
   </footer>
   <!-- Footer -->
--- src/components/HomeComp/HomeSlider.vue
+++ src/components/HomeComp/HomeSlider.vue
@@ -1,12 +1,56 @@
 <template>
-  <div class="slider">
-    <img class="one" src="@/assets/imgas/slider.png" alt="" />
+  <div class="home_slider">
+    <swiper
+      :pagination="pagination"
+      :direction="'vertical'"
+      :slidesPerView="1"
+      :spaceBetween="30"
+      :mousewheel="true"
+      :autoplay="{
+        delay: 2500,
+        disableOnInteraction: false,
+      }"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide>
+        <img src="@/assets/imgas/slider.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="@/assets/imgas/slider1.png" alt="" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="@/assets/imgas/slider2.png" alt="" />
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import "swiper/css";
+
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+
 export default {
   name: "HomeSlider",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Pagination, Autoplay],
+      pagination: {
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+      },
+    };
+  },
 };
 </script>
 
